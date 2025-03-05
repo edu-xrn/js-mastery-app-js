a67524d554b358e2aee6869236bee2d9d9fcaf59
lima8002 <57651553+lima8002@users.noreply.github.com>
2025-03-05 18:42:47 +0000

    feat: movie search

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
@@ -18,11 +19,14 @@ const App = () => {
   const [movieList, setMovieList] = useState([])
   const [isLoading, setIsLoading] = useState(false)
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = '') => {
     setIsLoading(true)
     setErrorMessage('')
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
       const response = await fetch(endpoint, API_OPTIONS)
 
       if (!response.ok) {
@@ -48,15 +52,15 @@ const App = () => {
   }
 
   useEffect(() => {
-    fetchMovies()
-  }, [])
+    fetchMovies(searchTerm)
+  }, [searchTerm])
   return (
     <main>
       <div className="pattern"/>
 
       <div className="wrapper">
         <header>
-          <img src="../public/hero.png" alt="Hero Banner"/>
+          <img src="/hero.png" alt="Hero Banner"/>
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
@@ -71,7 +75,7 @@ const App = () => {
           ) : (
             <ul>
               {movieList.map((movie) => (
-                <p className="text-white" key={movie.id}>{movie.title}</p>
+                <MovieCard movie={movie} key={movie.id}/>
               ))}
             </ul>
           )}
